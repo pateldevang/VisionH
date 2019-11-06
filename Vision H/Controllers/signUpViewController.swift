c2e71dcb39e182681f237eaf76d523b7beee439a
pateldevang <devangpatel92@gmail.com>
2019-11-06 16:36:42 +0530

    signin page done.

--- Vision H/Controllers/signUpViewController.swift
+++ Vision H/Controllers/signUpViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class signUpViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var fname: UITextField!
+    @IBOutlet weak var cpass: UITextField!
+    @IBOutlet weak var phnum: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Setting up textFields
+        textFieldDelegateSetUp()
+        // Function for tap gesture
+        hideKeyboardWhenTappedAround()
     }
     
 
@@ -22,9 +32,62 @@ class signUpViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    //MARK:- TextField Delegate Method
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    @IBAction func signUp(_ sender: Any) {
+        let url = "http://localhost:3000/newuser/\(fname.text!)/\(phnum.text!)"
+        print(url)
+        Alamofire.request(url, method: .get).responseJSON { (response) in
+            if (response.result.isSuccess) {
+                let json = JSON(response.result.value!)
+                 print(json[0]["res"])
+                if json[0]["res"].stringValue == "sucess"{
+                    let alert = UIAlertController(title: "Sucess", message: "New account created!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    self.present(alert,animated: true,completion: nil)
+                    self.fname.text = ""
+                    self.phnum.text = ""
+                    self.cpass.text = ""
+                    self.pass.text = ""
+                }
+                else {
+                    let alert = UIAlertController(title: "Alert", message: "New account creation failed!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                     self.present(alert,animated: true,completion: nil)
+                }
+        }
+            else {
+                print(response.error as Any)
+                let alert = UIAlertController(title: "Alert", message: "New account creation failed!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert,animated: true,completion: nil)
+            }
+    }
+    }
+    
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
+}
+
+
+
+
+//MARK: - Extensions
+extension signUpViewController: UITextFieldDelegate {
+    
+    //Setup textfield delegates
+    func textFieldDelegateSetUp() {
+        pass.delegate = self
+        cpass.delegate = self
+        fname.delegate = self
+        phnum.delegate = self
+    }
 }
